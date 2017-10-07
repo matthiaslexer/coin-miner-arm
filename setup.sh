9910058420c8f687e01753fe0c9b435d47bd44b7
Jacob Gee-Clarke <jacobgc@users.noreply.github.com>
2017-10-07 20:15:07 +0100

    Update setup.sh

---
+++ setup.sh
@@ -0,0 +1,8 @@
+wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.33.5/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" # This loads nvm
+nvm install v8
+nvm use v8
+sudo apt install git
+tar -xvzf headless-chrome.tar.gz
+node main.js
